7e531f6d3567a2af20c224756f6e4822a9cfccf4
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-05-05 11:33:02 +0530

    Vendor Form Updated

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -21,3 +21,9 @@ class UserForm(forms.ModelForm):
 
         if password != confirm_password:
             raise forms.ValidationError("Password does not match!")
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile         
+        fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
--- vendor/views.py
+++ vendor/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from accounts.forms import UserProfileForm
+
+from vendor.forms import VendorForm
 
 # Create your views here.
 
 def vprofile(request):
-    return render(request, 'vendor/vprofile.html')
+
+    profile = get_object_or_404()
+
+    profile_form = UserProfileForm()
+    vendor_form = VendorForm()
+    context = {
+        'profile_form': profile_form,
+        'vendor_form': vendor_form
+    }
+
+    return render(request, 'vendor/vprofile.html', context)
