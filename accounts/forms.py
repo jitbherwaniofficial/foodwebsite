ce77900f0bbc8bc53105ccb5945090809964152e
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-05-07 20:18:02 +0530

    validators and address line updated

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,13 +26,14 @@ class UserForm(forms.ModelForm):
 
 
 class UserProfileForm(forms.ModelForm):
+    address = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'start typing...', 'required':'required'}))
     profile_picture = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_images_validator])
     cover_photo = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}), validators=[allow_only_images_validator])
     # latitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
     # longitude = forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
     class Meta:
         model = UserProfile         
-        fields = ['profile_picture', 'cover_photo', 'address_line_1', 'address_line_2', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
+        fields = ['profile_picture', 'cover_photo', 'address', 'country', 'state', 'city', 'pin_code', 'latitude', 'longitude']
 
     # VERY VERY USEFUL METHOD FOR BIG ORGANIZATIONS
     def __init__(self, *args, **kwargs):
--- accounts/validators.py
+++ accounts/validators.py
@@ -4,6 +4,6 @@ from django.core.exceptions import ValidationError
 def allow_only_images_validator(value):
     ext = os.path.splitext(value.name)[1]
     print(ext)
-    valid_extension = ['.png', '.jpg', '.jpeg','.pdf']
+    valid_extension = ['.png', '.jpg', '.jpeg',]
     if not ext.lower() in valid_extension:
         raise ValidationError('Unsupported File Extension. Allowed extensions: '+ str(valid_extension))
