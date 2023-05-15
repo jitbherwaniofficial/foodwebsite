3fd5505ec3a7df977ef5109b2034580aab1f11db
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-05-15 12:07:35 +0530

    Delete Food Item Updated

--- vendor/urls.py
+++ vendor/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     # FOOD ITEM CRUD 
     path('menu_builder/food/add/', views.add_food, name='add_food'),
     path('menu_builder/food/edit/<int:pk>/', views.edit_food, name='edit_food'),
+    path('menu_builder/food/delete/<int:pk>/', views.delete_food, name='delete_food'),
 ]
