7cd27e7aeeab7f7a9ccf253fdbc8054236d2c525
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-05-17 12:31:57 +0530

    vendor detail updated and cover photo updated

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from vendor.models import Vendor
 
@@ -15,5 +15,10 @@ def marketplace(request):
     return render(request, 'marketplace/listings.html',context)
 
 
-def vendor_detail(request):
-    return render(request, 'marketplace/vendor_detail.html')
+def vendor_detail(request,vendor_slug):
+    vendor = get_object_or_404(Vendor, vendor_slug=vendor_slug)
+
+    context = {
+        'vendor':vendor
+    }
+    return render(request, 'marketplace/vendor_detail.html', context)
