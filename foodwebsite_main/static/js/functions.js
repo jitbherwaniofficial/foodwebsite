5f82ff7ed7790056eafada22bf0dcde7f5461693
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-05-20 14:52:01 +0530

    Places the quantity on each food item

--- foodwebsite_main/static/js/functions.js
+++ foodwebsite_main/static/js/functions.js
@@ -606,4 +606,11 @@ $(document).ready(function(){
             }
         })
     })
+
+    // place the cart item quantity on load
+    $('.item-qty').each(function () {
+        var the_id = $(this).attr('id')
+        var qty = $(this).attr('data-qty')
+        $('#' + the_id).html(qty)
+    })
 }) 
