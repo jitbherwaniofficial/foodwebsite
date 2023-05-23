6fc14854d33bdbd6b119c1154ad9d4de3b1e9b53
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-05-23 14:12:20 +0530

    Basic Search For Restaurant

--- marketplace/views.py
+++ marketplace/views.py
@@ -123,6 +123,13 @@ def search(request):
     latitude = request.GET['lat']
     longitude = request.GET['lng']
     radius = request.GET['radius']
-    r_name = request.GET['rest_name']
-    
-    return render(request, 'marketplace/listings.html')
+    keyword = request.GET['keyword']
+
+    vendors = Vendor.objects.filter(vendor_name__icontains=keyword, is_approved=True, user__is_active=True)
+    vendor_count = vendors.count()
+    context = {
+        'vendors': vendors,
+        'vendor_count': vendor_count
+    }
+
+    return render(request, 'marketplace/listings.html', context)
