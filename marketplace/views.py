f80b0d59179ff35ea85d0afdd8d4cf9dfb91fd27
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-05-27 19:42:05 +0530

    dropdown opening hours frontend

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,10 +1,11 @@
+from datetime import date
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from marketplace.context_processors import get_cart_amounts, get_cart_counter
 from marketplace.models import Cart
 from menu.models import Category, FoodItem
 
-from vendor.models import Vendor
+from vendor.models import OpeningHour, Vendor
 from django.db.models import Prefetch
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
@@ -34,6 +35,14 @@ def vendor_detail(request,vendor_slug):
         queryset= FoodItem.objects.filter(is_available=True)
         )
     )
+
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', 'from_hour')
+
+    # CHECK CURRENT DAY'S OPENING HOURS
+    today = date.today().isoweekday()
+    current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
+    print(current_opening_hours)
+
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
@@ -41,7 +50,9 @@ def vendor_detail(request,vendor_slug):
     context = {
         'vendor':vendor,
         'categories':categories,
-        'cart_items':cart_items
+        'cart_items':cart_items,
+        'opening_hours':opening_hours,
+        'current_opening_hours':current_opening_hours
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 
