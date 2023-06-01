c32dffa82a29972ee2dee0ad5d709032973d3ec5
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-01 13:39:43 +0530

    Profile Form and User Form Updated

--- customers/views.py
+++ customers/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from accounts.forms import UserInfoForm, UserProfileForm
 
 # Create your views here.
 
-
+@login_required(login_url='login')
 def cprofile(request):
-    return render(request, 'customer/cprofile.html')
+    profile_form = UserProfileForm()
+    user_form = UserInfoForm()
+
+    context = {
+        'profile_form': profile_form,
+        'user_form' : user_form,
+    }
+
+    return render(request, 'customer/cprofile.html', context)
