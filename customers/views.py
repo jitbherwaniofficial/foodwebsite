85afa27e5d5e9bc6c7f69e82be659dea9575a00f
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-01 14:03:33 +0530

    PrePopulated Forms

--- customers/views.py
+++ customers/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from accounts.forms import UserInfoForm, UserProfileForm
+from accounts.models import UserProfile
 
 # Create your views here.
 
 @login_required(login_url='login')
 def cprofile(request):
-    profile_form = UserProfileForm()
-    user_form = UserInfoForm()
+    profile = get_object_or_404(UserProfile, user=request.user)
+    profile_form = UserProfileForm(instance=profile)
+    user_form = UserInfoForm(instance=request.user)
 
     context = {
         'profile_form': profile_form,
