9c899ccd6ef4fb65624071b4bd4068844734b9c5
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-03 16:08:20 +0530

    6. Prepopulate Billing Form

--- marketplace/views.py
+++ marketplace/views.py
@@ -14,6 +14,7 @@ from django.db.models import Q
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D  # ``D`` is a shortcut for ``Distance``
 from django.contrib.gis.db.models.functions import Distance
+from accounts.models import UserProfile
 
 # Create your views here.
 
@@ -167,12 +168,26 @@ def search(request):
         return render(request, 'marketplace/listings.html', context)
     
 
+@login_required(login_url='login')
 def checkout(request):
     cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
     cart_count = cart_items.count()
     if cart_count <= 0:
         return redirect('marketplace')
-    form = OrderForm()
+    
+    user_profile = UserProfile.objects.get(user=request.user)
+    default_values = {
+        'first_name':request.user.first_name,
+        'last_name':request.user.last_name,
+        'phone':request.user.phone_number,
+        'email':request.user.email,
+        'address':user_profile.address,
+        'country':user_profile.country,
+        'state':user_profile.state,
+        'city':user_profile.city,
+        'pin_code':user_profile.pin_code,
+    }
+    form = OrderForm(initial=default_values)
     context = {
         'form':form,
         'cart_items':cart_items
