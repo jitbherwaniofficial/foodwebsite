3a2f733b98a0ac124018ed3b79fb0c1697a7e018
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-04 14:25:46 +0530

    Paypal Updated

--- foodwebsite_main/settings.py
+++ foodwebsite_main/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'marketplace.context_processors.get_cart_counter',
                 'marketplace.context_processors.get_cart_amounts',
                 'accounts.context_processors.get_user_profile',
+                'accounts.context_processors.get_paypal_client_id',
             ],
         },
     },
@@ -172,3 +173,5 @@ os.environ['PROJ_LIB'] = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\dat
 GDAL_LIBRARY_PATH = os.path.join(BASE_DIR, 'env\Lib\site-packages\osgeo\gdal304.dll')
 
 PAYPAL_CLIENT_ID = config('PAYPAL_CLIENT_ID')
+
+SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
