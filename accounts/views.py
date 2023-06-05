b4c7412268ea95523adb96ba8c347f5574bae391
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-05 21:04:07 +0530

    Calculate Total Revenue Per Vendor

--- accounts/views.py
+++ accounts/views.py
@@ -186,12 +186,20 @@ def custdashboard(request):
 def vendordashboard(request):
     vendor = Vendor.objects.get(user=request.user)
     orders = Order.objects.filter(vendors__in=[vendor.id], is_ordered=True).order_by('created_at')
-    recent_orders = orders[:5]
+    recent_orders = orders[:10]
+
+    # TOTAL REVENUE
+    total_revenue = 0
+    for i in orders:
+        total_revenue += i.get_total_by_vendor()['grand_total']
+
     context = {
         'orders':orders,
         'orders_count':orders.count(),
-        'recent_orders':recent_orders
+        'recent_orders':recent_orders,
+        'total_revenue':total_revenue,
     }
+
     return render(request, 'accounts/vendordashboard.html', context)
 
 
