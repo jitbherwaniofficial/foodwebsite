8b9bb10f7ac3bf70c5e31388003debff1aaa3b65
jitbherwaniofficial <jitbherwani92@gmail.com>
2023-06-06 14:05:35 +0530

    Mini Updates

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,17 +3,18 @@ from django.dispatch import receiver
 from .models import UserProfile,User
 
 
+
 @receiver(post_save, sender=User)
 def post_save_create_profile_receiver(sender, instance, created, **kwargs):
     print(created)
-    if created: 
+    if created:
         UserProfile.objects.create(user=instance)
-    else :
+    else:
         try:
             profile = UserProfile.objects.get(user=instance)
             profile.save()
         except:
-            #create the userprofile if not exist   
+            # Create the userprofile if not exist
             UserProfile.objects.create(user=instance)
 
 # post_save.connect(post_save_create_profile_receiver,sender=User)
